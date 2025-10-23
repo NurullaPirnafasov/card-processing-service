cf594144faa4471a98d8617715a7f8ab69b16885
NurullaPirnafasov <nurullapirnafasov@gmail.com>
2025-10-23 16:26:56 +0500

    it has all dependency is needed

---
+++ src/main/java/com/example/cardprocessingservice/CardProcessingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.cardprocessingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardProcessingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardProcessingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cardprocessingservice/CardProcessingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cardprocessingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardProcessingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
